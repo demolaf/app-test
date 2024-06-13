d184fc41babc7b5d71e066c9a7b728cece5fe40c
Ademola Fadumo <demolafadumo@gmail.com>
2024-06-13 18:52:35 +0100

    Initial Commit

---
+++ care-saas-test/care_saas_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  care_saas_testApp.swift
+//  care-saas-test
+//
+//  Created by Ademola Fadumo on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct care_saas_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
